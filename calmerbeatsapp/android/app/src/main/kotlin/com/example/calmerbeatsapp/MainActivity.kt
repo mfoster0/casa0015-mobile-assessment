2938f94002c5552921ccfe342091486f2caa89d9
mfoster0 <mark.foster@live.co.uk>
2024-04-16 19:10:32 +0100

    Testing with clean app
    Not a lot of useful things from these authentication tests

---
+++ calmerbeatsapp/android/app/src/main/kotlin/com/example/calmerbeatsapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.calmerbeatsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ calmerbeatsapp/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:calmerbeatsapp/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const MultiAuth(
+
+    ),
+  );
+}
+
